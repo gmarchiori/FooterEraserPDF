8872117260e6e704a5f0d0746b56c0a141a60284
Giacomo Marchiori <giacomo.marchiori@gmail.com>
2014-12-10 12:41:22 +0100

    test

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,11 +9,17 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("AAA");
 		
+		method1();
+		
 		JFrame frame = new JFrame("FrameDemo");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
 		
+	}
+	
+	private static boolean method1(){
+		return true;
 	}
 
 }
