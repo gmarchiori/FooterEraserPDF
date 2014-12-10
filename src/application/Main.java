a517239ae339595dbe2d1d9f0c3979a38ee2386a
Giacomo Marchiori <giacomo.marchiori@gmail.com>
2014-12-10 17:26:43 +0100

    test Tasks

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,19 +7,15 @@ public class Main {
 	public static void main(String[] args) {
 		
 		//TODO: xxx
+		//FIXME: aaa
+		//XXX: asd
 		System.out.println("AAA");
-		
-		method1();
 		
 		JFrame frame = new JFrame("FrameDemo");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
 		
-	}
-	
-	private static boolean method1(){
-		return true;
 	}
 
 }
