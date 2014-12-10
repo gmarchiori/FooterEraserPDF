6d11a9727884c5219008f4e09a4f403f4d17c7c9
Giacomo Marchiori <giacomo.marchiori@gmail.com>
2014-12-10 12:38:49 +0100

    fix application

---
+++ src/application/Main.java
@@ -0,0 +1,19 @@
+package application;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.println("AAA");
+		
+		JFrame frame = new JFrame("FrameDemo");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+
+}
