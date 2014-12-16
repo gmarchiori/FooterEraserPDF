1455bc5f41e47ff837f89920833fed0bf0156c58
Giacomo Marchiori <giacomo.marchiori@gmail.com>
2014-12-16 17:44:07 +0100

    implementazione JFrame e cancellazione footer PDF

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,16 @@
 package application;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileOutputStream;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 import com.itextpdf.text.Image;
 import com.itextpdf.text.pdf.PdfContentByte;
@@ -10,28 +20,77 @@ import com.itextpdf.text.pdf.PdfStamper;
 public class Main {
 	
 	public static void main(String[] args) {
-		try{
-			PdfReader pdfReader = new PdfReader("D://ZZZ//test.pdf");
+		
+		String path = "D:/ZZZ/";
+		String finalPath = "modified/";
+		String fileName = "test.pdf";
+		String imgName = "blank.png";
+		
+		int fromPage = 1;
+		int toPage;
+		
+		JFrame frame = new JFrame("FooterEraserPDF");
+		final JButton eraseButton = new JButton("Cancella footer");
+		JPanel statusPanel = new JPanel();
+		final JLabel statusLabel = new JLabel("Footer Eraser PDF");
+		
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setAlwaysOnTop(true);
+		frame.setSize(500, 500);
+		
+		statusPanel.setPreferredSize(new Dimension(500, 500));
+		statusPanel.setLayout(null);
 
-			PdfStamper pdfStamper = new PdfStamper(pdfReader, new FileOutputStream("D://ZZZ//test_MODIFIED.pdf"));
+		statusLabel.setLocation(150, 50);
+		statusLabel.setSize(statusLabel.getPreferredSize());
+		
+		eraseButton.setBounds(300, 50,	140, 50);
+		eraseButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				eraseButton.setLocation((int) eraseButton.getLocation().getX() + 20, (int) eraseButton.getLocation().getY() + 20);
+				statusLabel.setText(String.valueOf(System.nanoTime()));
+			}
+		});
+		
+		statusPanel.add(statusLabel);
+		statusPanel.add(eraseButton);
+		frame.add(statusPanel);
+		frame.pack();
+		
+		try {
+			
+			File dir = new File(path + finalPath);
+			dir.mkdir();
+			
+			PdfReader pdfReader = new PdfReader(path + fileName);
 
-			Image image = Image.getInstance("D://ZZZ/blank.png");
+			PdfStamper pdfStamper = new PdfStamper(pdfReader, new FileOutputStream(path + finalPath + fileName));
 
-			for(int i = 1; i <= pdfReader.getNumberOfPages(); i++){
+			Image image = Image.getInstance("img/" + imgName);
+			
+			toPage = pdfReader.getNumberOfPages();
+
+			for(int i = fromPage; i <= toPage; i++){
 
 				PdfContentByte content = pdfStamper.getOverContent(i);
 
-				image.setAbsolutePosition(275f, 35f);
+				image.setAbsolutePosition(0f, 20f);
 
 				content.addImage(image);
 			}
 
 			pdfStamper.close();
-
-			System.out.println("Success!");
 			
-		}catch (Exception e){
+			statusLabel.setText("Success!");
+			
+		} catch (Exception e) {
+			
 			e.printStackTrace();
+			
+			statusLabel.setText(e.toString());
+			
 		}
 	}
 	
