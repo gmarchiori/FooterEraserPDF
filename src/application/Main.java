3eca5480ec7106fa6cde991a90a318d059e91865
Giacomo Marchiori <giacomo.marchiori@gmail.com>
2014-12-16 09:07:51 +0100

    a

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,11 +3,12 @@ package application;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-import beans.prova1;
+import beans.Prova1;
 
 public class Main {
 	
 	/** 
+	 * prova javadoc 
 	 * @param args il parametro principale
 	 * @return void
 	 */
@@ -20,7 +21,7 @@ public class Main {
 		
 		System.out.println("AAA");
 		
-		prova1 p = new prova1();
+		Prova1 p = new Prova1();
 		p.setNum(22);
 		
 		JFrame frame = new JFrame("FrameDemo");
@@ -29,7 +30,7 @@ public class Main {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setAlwaysOnTop(true);
-		a
+		
 	}
 
 }
